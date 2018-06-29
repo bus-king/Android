3bb5cf6bfcac3811ab7c9c671d93449fce998b58
SeonheeKim <dnwdjqldb@cnu.ac.kr>
2018-06-29 19:23:01 +0900

    db shema chang and createContents is working code

--- Django/busking_django/models.py
+++ Django/busking_django/models.py
@@ -1,12 +1,17 @@
 from django.db import models
 
 class Event(models.Model):
-    created = models.DateTimeField(auto_now_add=True)
-    title = models.CharField(max_length=100, blank=True, default='')
-    author = models.CharField(max_length=20, blank=True, default='')
-    pw = models.CharField(max_length=12, blank=True, default='')
-    content = models.TextField()
+
     id = models.IntegerField(primary_key=True)
+    userID = models.CharField(max_length=100, blank=True, default='')
+    showName = models.CharField(max_length=100, blank=True, default='')
+    showTitle = models.CharField(max_length=100, blank=True, default='')
+    showLocation = models.CharField(max_length=100, blank=True, default='')
+    showGenre = models.PositiveSmallIntegerField(default=0)
+    showHeart = models.PositiveSmallIntegerField(default=0)
+    showTime = models.CharField(max_length=100, blank=True, default='')
+    showDescription = models.CharField(max_length=100, blank=True, default='')
+    created = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         ordering = ('created',)
