1aba1c89d039ee181ac3c6930b97c0858c6c8a2b
SeonheeKim <dnwdjqldb@cnu.ac.kr>
2018-06-30 04:23:56 +0900

    signup을 위한 user 테이블 추가

--- Django/busking_django/serializers.py
+++ Django/busking_django/serializers.py
@@ -1,16 +1,17 @@
 from rest_framework import serializers
-from busking_django.models import Event
+from busking_django.models import Content
+from busking_django.models import Signup
 
-class EventSerializer(serializers.ModelSerializer):
+class ContentSerializer(serializers.ModelSerializer):
     # ModelSerializer 를 이용해서 아래와 같이 짧은 코드로 직렬화 필드를 정의할 수 있다
     class Meta:
-        model = Event
+        model = Content
         # fields = ('id','title','author','pw','content')
         fields = ('id', 'userID', 'showName', 'showTitle', 'showLocation', 'showGenre', 'showHeart', 'showTime', 'showDescription')
 
     # 신규 Bbs instance를 생성해서 리턴해준다
     def create(self, validated_data):
-        return Event.objects.create(**validated_data)
+        return Content.objects.create(**validated_data)
 
     # 생성되어 있는 Bbs instance 를 저장한 후 리턴해준다
     def update(self, instance, validated_data):
@@ -22,5 +23,27 @@ class EventSerializer(serializers.ModelSerializer):
         instance.showHeart = validated_data.get('showHeart', instance.showHeart)
         instance.showTime = validated_data.get('showTime', instance.showTime)
         instance.showDescription = validated_data.get('showDescription', instance.showDescription)
+        instance.save()
+        return instance
+
+class SignupSerializer(serializers.ModelSerializer):
+    # ModelSerializer 를 이용해서 아래와 같이 짧은 코드로 직렬화 필드를 정의할 수 있다
+    class Meta:
+        model = Signup
+        # fields = ('id','title','author','pw','content')
+        fields = ('userId', 'userPw', 'pwCheck', 'name', 'mail', 'phone')
+
+    # 신규 Bbs instance를 생성해서 리턴해준다
+    def create(self, validated_data):
+        return Signup.objects.create(**validated_data)
+
+    # 생성되어 있는 Bbs instance 를 저장한 후 리턴해준다
+    def update(self, instance, validated_data):
+        instance.userId = validated_data.get('userId', instance.userId)
+        instance.userPw = validated_data.get('userPw', instance.userPw)
+        instance.pwCheck = validated_data.get('pwCheck', instance.pwCheck)
+        instance.name = validated_data.get('name', instance.name)
+        instance.mail = validated_data.get('mail', instance.mail)
+        instance.phone = validated_data.get('phone', instance.phone)
         instance.save()
         return instance
--- Django/busking_django/views.py
+++ Django/busking_django/views.py
@@ -1,19 +1,38 @@
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from busking_django.models import Event
-from busking_django.serializers import EventSerializer
 
-# 요청 url 인 bbs/ 에 대해서 urls.py 에 정의된 view.bbs_list 가 호출된다.
+from busking_django.models import Content
+from busking_django.models import Signup
+
+from busking_django.serializers import ContentSerializer
+from busking_django.serializers import SignupSerializer
+
+
 @api_view(['GET', 'POST'])
-def event_list(request, format=None):
+def event_signup(request, format=None):
     if request.method == 'GET':
-        event = Event.objects.all()
-        serializer = EventSerializer(event, many=True) # many 값이 True 이면 다수의 데이터 instance를 직렬화할수 있다
+        event = Signup.objects.all()
+        serializer = SignupSerializer(event, many=True) # many 값이 True 이면 다수의 데이터 instance를 직렬화할수 있다
         return Response(serializer.data)
 
     elif request.method == 'POST':
-        serializer = EventSerializer(data=request.data)
+        serializer = SignupSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+# 요청 url 인 bbs/ 에 대해서 urls.py 에 정의된 view.bbs_list 가 호출된다.
+@api_view(['GET', 'POST'])
+def event_contents(request, format=None):
+    if request.method == 'GET':
+        event = Content.objects.all()
+        serializer = ContentSerializer(event, many=True) # many 값이 True 이면 다수의 데이터 instance를 직렬화할수 있다
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = ContentSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
@@ -23,16 +42,16 @@ def event_list(request, format=None):
 @api_view(['GET', 'PUT', 'DELETE'])
 def event_detail(request, pk, format=None):
     try:
-        event = Event.objects.get(pk=pk)
-    except Event.DoesNotExist:
+        event = Content.objects.get(pk=pk)
+    except Content.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
 
     if request.method == 'GET':
-        serializer = EventSerializer(bbs)
+        serializer = ContentSerializer(bbs)
         return Response(serializer.data)
 
     elif request.method == 'PUT':
-        serializer = EventSerializer(event, data=request.data)
+        serializer = ContentSerializer(event, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
